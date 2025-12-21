4e5e18419508094a54f8750001d43644beb6bb10
ilyaasomar <ilyaascumar10@gmail.com>
2025-12-21 12:50:55 +0100

    attempt is starting

--- app/(dashboard)/(routes)/options/[optionId]/page.tsx
+++ app/(dashboard)/(routes)/options/[optionId]/page.tsx
@@ -1,32 +1,3 @@
-// import { auth } from "@/lib/auth";
-// import prisma from "@/lib/prisma";
-// import { headers } from "next/headers";
-// import { OptionForm } from "./form";
-
-// const OptionSubPage = async ({ params }: { params: { optionId: string } }) => {
-//   const session = await auth.api.getSession({ headers: await headers() });
-//   const userId = session?.user.id;
-//   const { optionId } = await params;
-//   console.log(optionId);
-//   let option;
-//   if (optionId !== "create") {
-//     option = await prisma.option.findFirst({
-//       where: { id: optionId, user_id: userId },
-//       include: { question: true, quiz: true },
-//     });
-//   }
-
-//   const quiz = await prisma.quiz.findMany({ where: { user_id: userId } });
-
-//   return (
-//     <div className="gap-4 p-4 md:gap-8 md:p-6">
-//       <OptionForm initialData={option} quiz={quiz} />
-//     </div>
-//   );
-// };
-
-// export default OptionSubPage;
-
 import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import { headers } from "next/headers";
